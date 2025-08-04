82b6dd07a165badd8e66fe5984cee20f0fef5c38
maaclin <ysolom90@gmail.com>
2025-08-04 14:41:17 +0100

    before modules

---
+++ terraform/providers.tf
@@ -0,0 +1,26 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "ysolomprojects-tfstate"
+    key          = "terraform"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
